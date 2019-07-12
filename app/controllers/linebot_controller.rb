a3c356e84e7e17543c33765e172506efb4ab714b
hnmtk <50006387@users.noreply.github.com>
2019-07-12 17:47:21 +0900

    Local weather

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -39,18 +39,25 @@ class LinebotController < ApplicationController
             input_prefecture = event.message['text']
             number = prefecture.find{|x| x[:name].include?(input_prefecture)}[:num]
             name = prefecture.find{|x| x[:name].include?(input_prefecture)}[:name]
+            point = prefecture.find{|x| x[:name].include?(input_prefecture)}[:point]
             url  = "https://www.drk7.jp/weather/xml/"+number+".xml"
             xml  = open( url ).read.toutf8
             doc = REXML::Document.new(xml)
-            xpath = 'weatherforecast/pref/area'
-            
-            area = doc.elements[xpath].attributes['id']
-            maxtemp = doc.elements[xpath + '/info/temperature/range'].text
-            mintemp = doc.elements[xpath + '/info/temperature/range[2]l'].text
-            per6to12 = doc.elements[xpath + '/info/rainfallchance/period[2]l'].text
-            per12to18 = doc.elements[xpath + '/info/rainfallchance/period[3]l'].text
-            per18to24 = doc.elements[xpath + '/info/rainfallchance/period[4]l'].text
-            push = "#{name}はね〜\n#{area}の降水確率はこんな感じ！\n    6〜12時  #{per6to12}％\n  12〜18時  #{per12to18}％\n  18〜24時  #{per18to24}％\nそれから気温は #{maxtemp}~#{mintemp} °C くらいだよ"
+            i = 1
+            array = []
+            while i <= point do
+              xpath = 'weatherforecast/pref/area['+i+']'
+              area = doc.elements[xpath].attributes['id']
+              maxtemp = doc.elements[xpath + '/info/temperature/range'].text
+              mintemp = doc.elements[xpath + '/info/temperature/range[2]l'].text
+              per6to12 = doc.elements[xpath + '/info/rainfallchance/period[2]l'].text
+              per12to18 = doc.elements[xpath + '/info/rainfallchance/period[3]l'].text
+              per18to24 = doc.elements[xpath + '/info/rainfallchance/period[4]l'].text
+              array << "#{area}の降水確率はこんな感じ！\n    6〜12時  #{per6to12}％\n  12〜18時  #{per12to18}％\n  18〜24時  #{per18to24}％\nそれから気温は #{maxtemp}~#{mintemp} °C くらいだよ\n"
+              i += 1
+            end
+            push = array
+            # "#{name}はね〜\n#{area}の降水確率はこんな感じ！\n    6〜12時  #{per6to12}％\n  12〜18時  #{per12to18}％\n  18〜24時  #{per18to24}％\nそれから気温は #{maxtemp}~#{mintemp} °C くらいだよ"
 
           when /.*(今日|きょう).*/
             maxtemp = doc.elements[xpath + 'info/temperature/range'].text
